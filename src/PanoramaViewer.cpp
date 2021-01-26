ab8b20bc0bcc1f65b6e905c13679d675a7a94784
Thomas Zorroche <thomas.zorroche@gmail.com>
2021-01-26 15:22:13 +0100

    [Panorama Viewer] Panorama cpp / hpp  : code refactoring

--- src/PanoramaViewer.cpp
+++ src/PanoramaViewer.cpp
@@ -27,106 +27,14 @@ PanoramaViewer::PanoramaViewer(QQuickItem* parent)
     : QQuickItem(parent)
 {
     setFlag(QQuickItem::ItemHasContents, true);
-    connect(this, &PanoramaViewer::gammaChanged, this, &PanoramaViewer::update);
-    connect(this, &PanoramaViewer::gainChanged, this, &PanoramaViewer::update);
     connect(this, &PanoramaViewer::textureSizeChanged, this, &PanoramaViewer::update);
     connect(this, &PanoramaViewer::sourceSizeChanged, this, &PanoramaViewer::update);
-    connect(this, &PanoramaViewer::channelModeChanged, this, &PanoramaViewer::update);
-    connect(this, &PanoramaViewer::imageChanged, this, &PanoramaViewer::update);
-    connect(this, &PanoramaViewer::sourceChanged, this, &PanoramaViewer::reload);
-    connect(this, &PanoramaViewer::verticesChanged, this, &PanoramaViewer::update);
-    connect(this, &PanoramaViewer::gridColorChanged, this, &PanoramaViewer::update);
     connect(this, &PanoramaViewer::sfmChanged, this, &PanoramaViewer::update);
     connect(this, &PanoramaViewer::imagesDataChanged, this, &PanoramaViewer::update);
 }
 
 PanoramaViewer::~PanoramaViewer()
 {
-}
-
-void PanoramaViewer::setLoading(bool loading)
-{
-    if (_loading == loading)
-    {
-        return;
-    }
-    _loading = loading;
-    Q_EMIT loadingChanged();
-}
-
-
-QVector4D PanoramaViewer::pixelValueAt(int x, int y)
-{
-    if (!_image)
-    {
-        // qInfo() << "[QtAliceVision] PanoramaViewer::pixelValueAt(" << x << ", " << y << ") => no valid image";
-        return QVector4D(0.0, 0.0, 0.0, 0.0);
-    }
-    else if (x < 0 || x >= _image->Width() ||
-        y < 0 || y >= _image->Height())
-    {
-        // qInfo() << "[QtAliceVision] PanoramaViewer::pixelValueAt(" << x << ", " << y << ") => out of range";
-        return QVector4D(0.0, 0.0, 0.0, 0.0);
-    }
-    aliceVision::image::RGBAfColor color = (*_image)(y, x);
-    // qInfo() << "[QtAliceVision] PanoramaViewer::pixelValueAt(" << x << ", " << y << ") => valid pixel: " << color(0) << ", " << color(1) << ", " << color(2) << ", " << color(3);
-    return QVector4D(color(0), color(1), color(2), color(3));
-}
-
-void PanoramaViewer::reload()
-{
-    if (_clearBeforeLoad)
-    {
-        _image.reset();
-        _imageChanged = true;
-        Q_EMIT imageChanged();
-    }
-    _outdated = false;
-
-    if (!_source.isValid())
-    {
-        _image.reset();
-        _imageChanged = true;
-        Q_EMIT imageChanged();
-        return;
-    }
-
-    if (!_loading)
-    {
-        setLoading(true);
-
-        // async load from file
-        auto ioRunnable = new FloatImageIORunnable(_source);
-        connect(ioRunnable, &FloatImageIORunnable::resultReady, this, &PanoramaViewer::onResultReady);
-        QThreadPool::globalInstance()->start(ioRunnable);
-    }
-    else
-    {
-        _outdated = true;
-    }
-}
-
-void PanoramaViewer::onResultReady(QSharedPointer<FloatImage> image, QSize sourceSize, const QVariantMap& metadata)
-{
-    setLoading(false);
-
-    if (_outdated)
-    {
-        // another request has been made while io thread was working
-        _image.reset();
-        reload();
-        return;
-    }
-
-    _image = image;
-    _imageChanged = true;
-    Q_EMIT imageChanged();
-
-    _sourceSize = sourceSize;
-    Q_EMIT sourceSizeChanged();
-
-    _metadata = metadata;
-    Q_EMIT metadataChanged();
 }
 
 void PanoramaViewer::computeInputImages()
@@ -168,123 +76,7 @@ void PanoramaViewer::computeInputImages()
 
 QSGNode* PanoramaViewer::updatePaintNode(QSGNode* oldNode, QQuickItem::UpdatePaintNodeData* data)
 {
-    QVector4D channelOrder(0.f, 1.f, 2.f, 3.f);
-
     QSGGeometryNode* root = static_cast<QSGGeometryNode*>(oldNode);
-    QSGSimpleMaterial<ShaderData>* material = nullptr;
-
-    QSGGeometry* geometryLine = nullptr;
-    bool updateSfmData = false;
-
-    if (!root)
-    {
-        root = new QSGGeometryNode;
-        auto geometry = new QSGGeometry(QSGGeometry::defaultAttributes_TexturedPoint2D(), _surface.vertexCount(), _surface.indexCount());
-        geometry->setDrawingMode(GL_TRIANGLES);
-        geometry->setIndexDataPattern(QSGGeometry::StaticPattern);
-        geometry->setVertexDataPattern(QSGGeometry::StaticPattern);
-        root->setGeometry(geometry);
-        root->setFlags(QSGNode::OwnsGeometry);
-
-        material = ImageViewerShader::createMaterial();
-        root->setMaterial(material);
-        root->setFlags(QSGNode::OwnsMaterial);
-    }
-    else
-    {
-        _createRoot = false;
-        material = static_cast<QSGSimpleMaterial<ShaderData>*>(root->material());
-    }
-
-    if (_surface.hasSubsChanged())
-    {
-        // Re size root
-        if (root)
-        {
-            root->geometry()->allocate(_surface.vertexCount(), _surface.indexCount());
-            root->markDirty(QSGNode::DirtyGeometry);
-        }
-    }
-
-    // enable Blending flag for transparency for RGBA
-    material->setFlag(QSGMaterial::Blending, _channelMode == EChannelMode::RGBA);
-
-    // change channelOrder according to channelMode
-    switch (_channelMode)
-    {
-    case EChannelMode::R:
-        channelOrder = QVector4D(0.f, 0.f, 0.f, -1.f);
-        break;
-    case EChannelMode::G:
-        channelOrder = QVector4D(1.f, 1.f, 1.f, -1.f);
-        break;
-    case EChannelMode::B:
-        channelOrder = QVector4D(2.f, 2.f, 2.f, -1.f);
-        break;
-    case EChannelMode::A:
-        channelOrder = QVector4D(3.f, 3.f, 3.f, -1.f);
-        break;
-    }
-
-    bool updateGeometry = false;
-    material->state()->gamma = _gamma;
-    material->state()->gain = _gain;
-    material->state()->channelOrder = channelOrder;
-
-    if (_imageChanged)
-    {
-        if (_surface.isDistoViewerEnabled())
-        {
-            updateSfmData = true;
-        }
-
-        QSize newTextureSize;
-        auto texture = std::make_unique<FloatTexture>();
-        if (_image)
-        {
-            texture->setImage(_image);
-            texture->setFiltering(QSGTexture::Nearest);
-            texture->setHorizontalWrapMode(QSGTexture::Repeat);
-            texture->setVerticalWrapMode(QSGTexture::Repeat);
-            newTextureSize = texture->textureSize();
-        }
-        material->state()->texture = std::move(texture);
-
-        _imageChanged = false;
-
-        if (_textureSize != newTextureSize)
-        {
-            _textureSize = newTextureSize;
-            updateGeometry = true;
-            Q_EMIT textureSizeChanged();
-        }
-    }
-
-    const auto newBoundingRect = boundingRect();
-    if (updateGeometry || _boundingRect != newBoundingRect)
-    {
-        _boundingRect = newBoundingRect;
-
-        const float windowRatio = _boundingRect.width() / _boundingRect.height();
-        const float textureRatio = _textureSize.width() / float(_textureSize.height());
-        QRectF geometryRect = _boundingRect;
-        if (windowRatio > textureRatio)
-        {
-            geometryRect.setWidth(geometryRect.height() * textureRatio);
-        }
-        else
-        {
-            geometryRect.setHeight(geometryRect.width() / textureRatio);
-        }
-        geometryRect.moveCenter(_boundingRect.center());
-
-        static const int MARGIN = 0;
-        geometryRect = geometryRect.adjusted(MARGIN, MARGIN, -MARGIN, -MARGIN);
-
-        QSGGeometry::updateTexturedRectGeometry(root->geometry(), geometryRect, QRectF(0, 0, 1, 1));
-        root->markDirty(QSGNode::DirtyGeometry);
-    }
-
     return root;
 }
 
@@ -292,86 +84,11 @@ QSGNode* PanoramaViewer::updatePaintNode(QSGNode* oldNode, QQuickItem::UpdatePai
 *   Q_INVOKABLE Functions
 */
 
-QPoint PanoramaViewer::getVertex(int index)
-{
-    return _surface.vertex(index);
-}
-
-void PanoramaViewer::setVertex(int index, float x, float y)
-{
-    QPoint point(x, y);
-    _surface.vertex(index) = point;
-    _surface.verticesChanged(true);
-    _surface.gridChanged(true);
-    Q_EMIT verticesChanged(false);
-}
-
-void PanoramaViewer::displayGrid(bool display)
-{
-    _surface.gridChanged(true);
-    _surface.gridDisplayed(display);
-    Q_EMIT verticesChanged(false);
-}
-
-void PanoramaViewer::setGridColorQML(const QColor& color)
-{
-    _surface.setGridColor(color);
-    Q_EMIT gridColorChanged();
-}
-
-void PanoramaViewer::defaultControlPoints()
-{
-    _surface.clearVertices();
-    _surface.reinitialize(true);
-    _surface.verticesChanged(true);
-    _surface.gridChanged(true);
-    Q_EMIT verticesChanged(false);
-}
-
-void PanoramaViewer::resized()
-{
-    _surface.verticesChanged(true);
-    _surface.gridChanged(true);
-    Q_EMIT verticesChanged(false);
-}
-
-bool PanoramaViewer::reinit()
-{
-    return _surface.hasReinitialized();
-}
-
-void PanoramaViewer::hasDistortion(bool distortion)
-{
-    _distortion = distortion;
-    _imageChanged = true;
-    _surface.verticesChanged(true);
-    _surface.clearVertices();
-    Q_EMIT verticesChanged(false);
-
-}
-
-void PanoramaViewer::updateSubdivisions(int subs)
-{
-    _surface.subsChanged(true);
-    _surface.setSubdivisions(subs);
-
-    _surface.clearVertices();
-    _surface.verticesChanged(true);
-    _surface.gridChanged(true);
-    Q_EMIT verticesChanged(false);
-}
-
 void PanoramaViewer::setSfmPath(const QString& path)
 {
     _sfmPath = path;
     computeInputImages();
 }
-
-QPoint PanoramaViewer::getPrincipalPoint()
-{
-    return _surface.principalPoint();
-}
-
 
 
 }  // qtAliceVision
--- src/PanoramaViewer.hpp
+++ src/PanoramaViewer.hpp
@@ -30,41 +30,18 @@ namespace qtAliceVision
     {
         Q_OBJECT
             /// Path to image
-            Q_PROPERTY(QUrl source MEMBER _source NOTIFY sourceChanged)
-            /// 
-            Q_PROPERTY(float gamma MEMBER _gamma NOTIFY gammaChanged)
-            /// 
-            Q_PROPERTY(float gain MEMBER _gain NOTIFY gainChanged)
-            ///
             Q_PROPERTY(QSize textureSize MEMBER _textureSize NOTIFY textureSizeChanged)
             ///
             Q_PROPERTY(QSize sourceSize READ sourceSize NOTIFY sourceSizeChanged)
-            /// Whether the image is currently being loaded from file
-            Q_PROPERTY(bool loading READ loading NOTIFY loadingChanged)
-            /// Whether to clear image between two loadings
+
             Q_PROPERTY(bool clearBeforeLoad MEMBER _clearBeforeLoad NOTIFY clearBeforeLoadChanged)
 
-            Q_PROPERTY(EChannelMode channelMode MEMBER _channelMode NOTIFY channelModeChanged)
-
-            Q_PROPERTY(QVariantMap metadata READ metadata NOTIFY metadataChanged)
-
-            Q_PROPERTY(QList<QPoint> vertices READ vertices NOTIFY verticesChanged)
-
-            Q_PROPERTY(QColor gridColor READ gridColor NOTIFY gridColorChanged)
-            
             Q_PROPERTY(QVariantMap imagesData READ imagesData NOTIFY imagesDataChanged)
 
 
     public:
         explicit PanoramaViewer(QQuickItem* parent = nullptr);
         ~PanoramaViewer() override;
-
-        bool loading() const
-        {
-            return _loading;
-        }
-
-        void setLoading(bool loading);
 
         QSize sourceSize() const
         {
@@ -81,84 +58,32 @@ namespace qtAliceVision
             return _imagesData;
         }
 
-        QList<QPoint> vertices() const
-        {
-            return _surface.vertices();
-        }
-
-        QColor gridColor()
-        {
-            return _surface.gridColor();
-        }
-
-        enum class EChannelMode : quint8 { RGBA, RGB, R, G, B, A };
-        Q_ENUM(EChannelMode)
-
     public:
-        Q_SIGNAL void sourceChanged();
-        Q_SIGNAL void loadingChanged();
         Q_SIGNAL void clearBeforeLoadChanged();
-        Q_SIGNAL void gammaChanged();
-        Q_SIGNAL void gainChanged();
         Q_SIGNAL void textureSizeChanged();
         Q_SIGNAL void sourceSizeChanged();
-        Q_SIGNAL void channelModeChanged();
         Q_SIGNAL void imageChanged();
         Q_SIGNAL void metadataChanged();
-        Q_SIGNAL void verticesChanged(bool reinit);
-        Q_SIGNAL void gridColorChanged();
         Q_SIGNAL void sfmChanged();
         Q_SIGNAL void imagesDataChanged(const QVariantMap& imagesData);
         
-
-        Q_INVOKABLE QVector4D pixelValueAt(int x, int y);
-        Q_INVOKABLE QPoint getVertex(int index);
-        Q_INVOKABLE void setVertex(int index, float x, float y);
-        Q_INVOKABLE void displayGrid(bool display);
-        Q_INVOKABLE void setGridColorQML(const QColor& color);
-        Q_INVOKABLE void defaultControlPoints();
-        Q_INVOKABLE void resized();
-        Q_INVOKABLE bool reinit();
-        Q_INVOKABLE void hasDistortion(bool distortion);
-        Q_INVOKABLE void updateSubdivisions(int subs);
         Q_INVOKABLE void setSfmPath(const QString& path);
-        Q_INVOKABLE QPoint getPrincipalPoint();
 
 
     private:
-        /// Reload image from source
-        void reload();
-        /// Handle result from asynchronous file loading
-        Q_SLOT void onResultReady(QSharedPointer<qtAliceVision::FloatImage> image, QSize sourceSize, const QVariantMap& metadata);
         /// Custom QSGNode update
         QSGNode* updatePaintNode(QSGNode* oldNode, QQuickItem::UpdatePaintNodeData* data) override;
-        /// ...
         void computeInputImages();
 
     private:
-        QUrl _source;
-        float _gamma = 1.f;
-        float _gain = 1.f;
-
-        bool _loading = false;
-        bool _outdated = false;
         bool _clearBeforeLoad = true;
 
-        bool _imageChanged = false;
-        EChannelMode _channelMode;
-        QSharedPointer<FloatImage> _image;
-        QRectF _boundingRect;
         QSize _textureSize;
         QSize _sourceSize = QSize(0, 0);
 
         QVariantMap _metadata;
 
-        Surface _surface;
-        bool _distortion = false;
-        bool _createRoot = true;
-
         QString _sfmPath;
-        //std::map<std::string, aliceVision::IndexT> _imagesData;
         QVariantMap _imagesData;
     };
 
