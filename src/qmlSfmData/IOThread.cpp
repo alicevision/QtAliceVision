352086e1149c1608c81c2c62e9ef5caac3a1daff
Fabien SERVANT <servantf@frarmt5018.mikros.int>
2023-09-26 14:47:45 +0200

    New qmlSfmData

---
+++ src/qmlSfmData/IOThread.cpp
@@ -0,0 +1,42 @@
+#include "IOThread.hpp"
+#include <QFile>
+#include <QDebug>
+
+namespace sfmdataentity
+{
+
+void IOThread::read(const QUrl& source)
+{
+    _source = source;
+    start();
+}
+
+void IOThread::run()
+{
+    // ensure file exists and is valid
+    if(!_source.isValid() || !QFile::exists(_source.toLocalFile()))
+        return;
+    QMutexLocker lock(&_mutex);
+    
+
+    if (!aliceVision::sfmDataIO::Load(_sfmData, _source.toLocalFile().toStdString(), aliceVision::sfmDataIO::ESfMData(aliceVision::sfmDataIO::ESfMData::VIEWS | aliceVision::sfmDataIO::ESfMData::INTRINSICS | aliceVision::sfmDataIO::ESfMData::EXTRINSICS |aliceVision::sfmDataIO::ESfMData::STRUCTURE)))
+    {
+        qDebug() << "[QtAliceVision] Failed to load sfmData: " << _source << ".";
+    }
+}
+
+void IOThread::clear()
+{
+    QMutexLocker lock(&_mutex);
+
+    _sfmData = aliceVision::sfmData::SfMData();
+}
+
+const aliceVision::sfmData::SfMData & IOThread::getSfmData() const
+{
+    // mutex is mutable and can be locked in const methods
+    QMutexLocker lock(&_mutex);
+    return _sfmData;
+}
+
+}
---
+++ src/qmlSfmData/SfmDataEntity.cpp
@@ -0,0 +1,203 @@
+#include "SfmDataEntity.hpp"
+#include "IOThread.hpp"
+
+#include "CameraLocatorEntity.hpp"
+#include "PointCloudEntity.hpp"
+
+#include <aliceVision/geometry/Pose3.hpp>
+
+#include <Qt3DRender/QEffect>
+#include <Qt3DRender/QTechnique>
+#include <Qt3DRender/QRenderPass>
+#include <Qt3DRender/QShaderProgram>
+#include <Qt3DRender/QObjectPicker>
+#include <Qt3DRender/QPickEvent>
+#include <Qt3DRender/QDebugOverlay>
+#include <Qt3DExtras/QPerVertexColorMaterial>
+#include <QFile>
+
+
+namespace sfmdataentity
+{
+
+SfmDataEntity::SfmDataEntity(Qt3DCore::QNode* parent)
+    : Qt3DCore::QEntity(parent)
+    , _pointSizeParameter(new Qt3DRender::QParameter)
+    , _ioThread(new IOThread())
+{
+    connect(_ioThread.get(), &IOThread::finished, this, &SfmDataEntity::onIOThreadFinished);
+    createMaterials();
+}
+
+void SfmDataEntity::setSource(const QUrl& value)
+{
+    if(_source == value)
+        return;
+    _source = value;
+    loadSfmData();
+    Q_EMIT sourceChanged();
+}
+
+void SfmDataEntity::setPointSize(const float& value)
+{
+    if(_pointSize == value)
+    {
+        return;
+    }
+
+    _pointSize = value;
+    _pointSizeParameter->setValue(value);
+    _cloudMaterial->setEnabled(_pointSize > 0.0f);
+
+    Q_EMIT pointSizeChanged();
+}
+
+void SfmDataEntity::setLocatorScale(const float& value)
+{
+    if(_locatorScale == value)
+    {
+        return;
+    }
+
+    _locatorScale = value;
+    scaleLocators();
+
+    Q_EMIT locatorScaleChanged();
+}
+
+void SfmDataEntity::scaleLocators() const
+{
+    for (auto* entity : _cameras)
+    {
+        for(auto* transform : entity->findChildren<Qt3DCore::QTransform*>())
+        {
+            transform->setScale(_locatorScale);
+        }
+    }
+}
+
+void SfmDataEntity::createMaterials()
+{
+    using namespace Qt3DRender;
+    using namespace Qt3DExtras;
+
+    _cloudMaterial = new QMaterial(this);
+    _cameraMaterial = new QPerVertexColorMaterial(this);
+
+    // configure cloud material
+    auto effect = new QEffect;
+    auto technique = new QTechnique;
+    auto renderPass = new QRenderPass;
+    auto shaderProgram = new QShaderProgram;
+
+    shaderProgram->setVertexShaderCode(R"(#version 130
+    in vec3 vertexPosition;
+    in vec3 vertexColor;
+    out vec3 color;
+    uniform mat4 mvp;
+    uniform mat4 projectionMatrix;
+    uniform mat4 viewportMatrix;
+    uniform float pointSize;
+    void main()
+    {
+        color = vertexColor;
+        gl_Position = mvp * vec4(vertexPosition, 1.0);
+        gl_PointSize = max(viewportMatrix[1][1] * projectionMatrix[1][1] * pointSize / gl_Position.w, 1.0);
+    }
+    )");
+
+    // set fragment shader
+    shaderProgram->setFragmentShaderCode(R"(#version 130
+        in vec3 color;
+        out vec4 fragColor;
+        void main(void)
+        {
+            fragColor = vec4(color, 1.0);
+        }
+    )");
+
+    // add a pointSize uniform
+    _pointSizeParameter->setName("pointSize");
+    _pointSizeParameter->setValue(_pointSize);
+    _cloudMaterial->addParameter(_pointSizeParameter);
+
+    // build the material
+    renderPass->setShaderProgram(shaderProgram);
+    technique->addRenderPass(renderPass);
+    effect->addTechnique(technique);
+    _cloudMaterial->setEffect(effect);
+}
+
+void SfmDataEntity::clear()
+{
+    // clear entity (remove direct children & all components)
+    auto entities = findChildren<QEntity*>(QString(), Qt::FindDirectChildrenOnly);
+    for(auto entity : entities)
+    {
+        entity->setParent((QNode*)nullptr);
+        entity->deleteLater();
+    }
+    for(auto& component : components())
+    {
+        removeComponent(component);
+    }
+    
+    _cameras.clear();
+    _pointClouds.clear();
+}
+
+// private
+void SfmDataEntity::loadSfmData()
+{
+    clear();
+
+    if(_source.isEmpty())
+    {
+        setStatus(SfmDataEntity::None);
+        return;
+    }
+
+    setStatus(SfmDataEntity::Loading);
+
+    _ioThread->read(_source);
+}
+
+void SfmDataEntity::onIOThreadFinished()
+{
+    const auto& sfmData = _ioThread->getSfmData();
+
+    Qt3DCore::QEntity * root = new Qt3DCore::QEntity(this);
+
+    {
+        PointCloudEntity* entity = new PointCloudEntity(root);
+        entity->setData(sfmData.getLandmarks());
+        entity->addComponent(_cloudMaterial);
+    }
+
+    for (const auto & pv : sfmData.getViews())
+    {
+        if(!sfmData.isPoseAndIntrinsicDefined(pv.second.get()))
+        {
+            continue;
+        }
+
+        CameraLocatorEntity* entity = new CameraLocatorEntity(pv.first, root);
+        entity->addComponent(_cameraMaterial);
+        entity->setTransform(sfmData.getPoses().at(pv.second->getPoseId()).getTransform().getHomogeneous());
+        entity->setObjectName(std::to_string(pv.first).c_str());
+    }
+
+    scaleLocators();
+
+    _cameras = findChildren<CameraLocatorEntity*>();
+    _pointClouds = findChildren<PointCloudEntity*>();
+    
+    setStatus(SfmDataEntity::Ready);
+
+    _ioThread->clear();
+    
+    Q_EMIT pointCloudsChanged();
+    Q_EMIT camerasChanged();
+}
+
+} // namespace
