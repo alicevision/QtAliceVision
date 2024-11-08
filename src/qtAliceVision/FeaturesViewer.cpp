e1e11719e0d2a6fabcf620b605e0aae5a93d15e2
Fabien Servant <fabien.servant@technicolor.com>
2024-11-08 15:40:11 +0100

    Update project function name

--- src/qtAliceVision/FeaturesViewer.cpp
+++ src/qtAliceVision/FeaturesViewer.cpp
@@ -713,7 +713,7 @@ void FeaturesViewer::updateReconstruction()
                 const auto& pose = sfmData.getPose(view);
                 const auto& camTransform = pose.getTransform();
                 const auto& intrinsic = sfmData.getIntrinsicPtr(view.getIntrinsicId());
-                const aliceVision::Vec2 reprojection = intrinsic->project(camTransform, landmark.X.homogeneous());
+                const aliceVision::Vec2 reprojection = intrinsic->transformProject(camTransform, landmark.X.homogeneous());
                 data.rx = static_cast<float>(reprojection.x());
                 data.ry = static_cast<float>(reprojection.y());
             }
